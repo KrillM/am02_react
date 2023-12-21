483dbbae1d5fd2e8cb4e522dc370389b059ead9c
KrillM <mks160615@gmail.com>
2023-12-21 11:00:02 +0900

    review react

---
+++ a_hello/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hello from './components/Hello';
+import Hi from './components/Hi';
+
+function App() {
+  return (<>
+    <Hello />
+    <Hi />
+  </>
+  );
+}
+
+export default App;
