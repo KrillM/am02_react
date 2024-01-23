cdb4c847aafa771ce45dfcba460e033726310114
명길식 <myeong-gilsig@MBook.local>
2024-01-23 20:50:59 +0900

    restudy react styling

--- i_style/src/App.js
+++ i_style/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import StyleCSS from './components/StyleCSS';
+import StyleModule from './components/StyleModule';
+import StyleSass from './components/StyleSass';
+import StyledComponents from './components/StyledComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <StyleCSS />
+    <br />
+    <StyleModule />
+    <br/>
+    <StyleSass />
+    <br/>
+    <StyledComponents />
+  </>);
 }
 
 export default App;
